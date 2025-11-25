41c5a5343d0e743f997fb8a01493ded2945b5e10
Clara Ortega <ortega.clara1331@gmail.com>
2025-11-25 15:57:54 +0100

    feat: send response

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const http = require('http');
 
 http.createServer((request, response) => {
-    console.log('Request received');
+    // Send the HTTP header
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    // Send the response body
+    response.end('Hello World\n');
 }).listen(3000);
 
 console.log('Server is running at http://127.0.0.1:3000/');
