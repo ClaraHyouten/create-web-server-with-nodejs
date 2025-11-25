5ac93c63221a3358b5fab4ef6ba9671d237e0447
Clara Ortega <ortega.clara1331@gmail.com>
2025-11-25 16:31:26 +0100

    feat: server retrieve informations

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const http = require('http');
+const { routes } = require('./routes.js');
+const { parseQuery, parseBody } = require('./utils.js');
 
-const routes = {
-    '/hello': function(request, response){
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.end('Hello World\n');
-    },
-    '/goodbye': function(request, response){
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.end('Goodbye World\n');
-    }
+async function middleware(request, response){
+    parseQuery(request, response);
+    await parseBody(request, response);
 };
 
-http.createServer((request, response) => {
-    const handler = routes[request.url];
-    if (handler) {
-        return handler(request, response);
+http.createServer(async (request, response) => {
+    const path = request.url.split('?')[0];
+    const handler = routes[path] ? routes[path][request.method] : null;
+
+    if(handler){
+        await middleware(request, response);
+	    handler(request, response);
     } else {
         response.writeHead(404, {'Content-Type': 'text/plain'});
         return response.end('404 Not Found\n');
