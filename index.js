dda84295aa1c96bc533e7033e174ccc20e4f9920
Clara Ortega <ortega.clara1331@gmail.com>
2025-11-25 15:55:41 +0100

    feat: create server

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const http = require('http');
+
+http.createServer((request, response) => {
+    console.log('Request received');
+}).listen(3000);
+
+console.log('Server is running at http://127.0.0.1:3000/');
