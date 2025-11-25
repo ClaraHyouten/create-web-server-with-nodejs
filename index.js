151c30f6e6984961fc0b6296ed93bf5b233640f6
Clara Ortega <ortega.clara1331@gmail.com>
2025-11-25 16:01:04 +0100

    feat: listen to the request

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const http = require('http');
 
-http.createServer((request, response) => {
-    // Send the HTTP header
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+const routes = {
+    '/hello': function(request, response){
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.end('Hello World\n');
+    },
+    '/goodbye': function(request, response){
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.end('Goodbye World\n');
+    }
+};
 
-    // Send the response body
-    response.end('Hello World\n');
+http.createServer((request, response) => {
+    const handler = routes[request.url];
+    if (handler) {
+        return handler(request, response);
+    } else {
+        response.writeHead(404, {'Content-Type': 'text/plain'});
+        return response.end('404 Not Found\n');
+    }
 }).listen(3000);
 
 console.log('Server is running at http://127.0.0.1:3000/');
